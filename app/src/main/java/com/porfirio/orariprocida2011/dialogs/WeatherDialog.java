0f63925baccd6ca92a06d57504aa81b431a5ecfe
CristianCarrella <72307833+CristianCarrella@users.noreply.github.com>
2025-03-08 11:09:36 +0100

    Weather dialog

---
+++ app/src/main/java/com/porfirio/orariprocida2011/dialogs/WeatherDialog.java
@@ -0,0 +1,37 @@
+package com.porfirio.orariprocida2011.dialogs;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.graphics.drawable.ColorDrawable;
+import android.view.Window;
+import android.widget.GridView;
+import android.widget.TextView;
+
+import com.porfirio.orariprocida2011.R;
+import com.porfirio.orariprocida2011.adapter.WindObservationAdapter;
+import com.porfirio.orariprocida2011.entity.Meteo;
+
+import java.util.Objects;
+
+public class WeatherDialog extends Dialog {
+
+    public WeatherDialog(Context context, String dateTime, String windInfo, Meteo meteo) {
+        super(context);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.weather_dialog);
+        Objects.requireNonNull(getWindow()).setBackgroundDrawableResource(android.R.color.transparent);
+
+        TextView tvDateTime = findViewById(R.id.tvDateTime);
+        TextView tvWindInfo = findViewById(R.id.tvWindInfo);
+        GridView windTable = findViewById(R.id.windTable);
+
+        tvWindInfo.setText(windInfo);
+        tvDateTime.setText(dateTime);
+
+        if (meteo.getForecasts() != null && !meteo.getForecasts().isEmpty()) {
+            WindObservationAdapter adapter = new WindObservationAdapter(getContext(), meteo.getForecasts());
+            windTable.setAdapter(adapter);
+        }
+    }
+}
